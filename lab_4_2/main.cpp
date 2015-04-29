0fab0bb4dcbd167d028e34a7b408b3623db4a338
cancel <sl888amg@gmail.com>
2015-04-29 16:08:16 +0300

    Initial Commit

---
+++ lab_4_2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  lab_4_2
+//
+//  Created by Nikita on 29.04.15.
+//  Copyright (c) 2015 Nikita. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
